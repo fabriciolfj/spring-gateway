b1b99b5689a12719e3cf6761e3e4adbec6f8c805
fabricio <fabricio.jacob@outlook.com>
2021-07-20 23:32:15 -0300

    add

--- src/main/java/com/github/fabriciolfj/edgeservice/security/SecurityConfig.java
+++ src/main/java/com/github/fabriciolfj/edgeservice/security/SecurityConfig.java
@@ -3,12 +3,16 @@ package com.github.fabriciolfj.edgeservice.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizedClientRepository;
 import org.springframework.security.oauth2.client.web.server.WebSessionServerOAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint;
@@ -19,7 +23,11 @@ import reactor.core.publisher.Mono;
 import java.util.Objects;
 
 @Configuration
+@EnableWebFluxSecurity
 public class SecurityConfig {
+
+    public static final String ACTUATOR = "/actuator/**";
+    public static final String SCOPE_CREDITO_LIMITE_TRANSACAO = "SCOPE_CoffeeAndITRole";
 
     /*
     * Armazenar o token e o token de atualizacao no redis
@@ -30,19 +38,56 @@ public class SecurityConfig {
     }
 
     @Bean
+    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http.
+                exceptionHandling()
+                .authenticationEntryPoint((serverWebExchange, e) ->
+                        Mono.fromRunnable(() ->
+                                serverWebExchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED)
+                        )
+                ).accessDeniedHandler((serverWebExchange, e) -> Mono.fromRunnable(() ->
+                serverWebExchange.getResponse().setStatusCode(HttpStatus.FORBIDDEN)
+        )).and()
+                ///.redirectToHttps().and()
+                .csrf().disable()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .authorizeExchange()
+                .pathMatchers(ACTUATOR, "/swagger-ui.html", "/webjars/**",
+                        "/swagger-resources/**", "/v2/api-docs/**", "/.well-known/**", "/", "/favicon.ico", "/swagger-ui/**").permitAll()
+                .pathMatchers("/v1/**", "/v2/**").access((authentication, object) -> authentication.flatMap(flatAuthentication ->
+                        Mono.just(new AuthorizationDecision(((JwtAuthenticationToken) flatAuthentication).getAuthorities()
+                                .contains(new SimpleGrantedAuthority(SCOPE_CREDITO_LIMITE_TRANSACAO))))
+
+                )
+
+
+        )
+                .anyExchange().authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+        return http.build();
+    }
+
+    /*
+    * essa forma o gateway faz o tramite de pegar o token com base no session do front
+    * */
+    /*@Bean
     public SecurityWebFilterChain springSecurityFilterChain(final ServerHttpSecurity http, final ReactiveClientRegistrationRepository repository) {
         return http
                 .authorizeExchange(e ->
                         e.pathMatchers("/","/.css", "/.js", "/favicon.ico").permitAll()
                                 .anyExchange().authenticated())
                 .exceptionHandling(exceptionHandlingSpec -> exceptionHandlingSpec
-                .authenticationEntryPoint(new HttpStatusServerEntryPoint(HttpStatus.UNAUTHORIZED)))
+                .authenticationEntryPoint(new HttpStatusServerEntryPoint(HttpStatus.UNAUTHORIZED))) //para nao retirecionar a pagina de login
                 .oauth2Login(Customizer.withDefaults())
+                .csrf().disable()
                 .logout(logoutSpec -> logoutSpec.logoutSuccessHandler(oidcLogoutSuccessHandler(repository)))
                 .build();
-    }
+    }*/
 
-    @Bean
+    /*@Bean
     WebFilter csrfWebFilter() {
         return (exchange, chain) -> {
             String key = CsrfToken.class.getName();
@@ -51,7 +96,7 @@ public class SecurityConfig {
                     .doOnSuccess(token -> {})
                     .then(chain.filter(exchange));
         };
-    }
+    }*/
 
     private ServerLogoutSuccessHandler oidcLogoutSuccessHandler(ReactiveClientRegistrationRepository clientRegistrationRepository) {
         var oidcLogoutSuccessHandler = new OidcClientInitiatedServerLogoutSuccessHandler(clientRegistrationRepository);
